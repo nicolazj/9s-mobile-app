e88a2e13cfe2db5d074b8c4c81dd7ba9bea07d41
Nicolas Jiang <nicolas.jiang@9spokes.com>
2018-11-02 14:25:45 +1300

    before tslint

--- src/Lock.ts
+++ src/Lock.ts
@@ -1,12 +1,12 @@
 import { Entity } from './types';
 class Lock<T> {
   private resolve?: (value?: T | PromiseLike<T> | undefined) => void;
-  hold() {
+  public hold() {
     return new Promise<T>(resolve => {
       this.resolve = resolve;
     });
   }
-  release(payload: T) {
+  public release(payload: T) {
     this.resolve && this.resolve(payload);
   }
 }
