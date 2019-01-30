99dbba0c05a168e9f959bf735ce6254d9d4679b2
Nicolas Jiang <nicolas.jiang@9spokes.com>
2019-01-30 15:43:48 +1300

    add sortable

--- src/screens/ManageWidgets.tsx
+++ src/screens/ManageWidgets.tsx
@@ -1,16 +1,15 @@
 import _groupby from 'lodash.groupby';
-import { Body, Left, List, ListItem, Right, Text } from 'native-base';
 import React from 'react';
 import { Image, ScrollView, StatusBar, View } from 'react-native';
 
 import { Ionicons } from '@expo/vector-icons';
-
+import SortableList from 'react-native-sortable-list';
 import agent from '../agent';
 import * as P from '../primitives';
 import { scale } from '../scale';
 import appState, { AppState } from '../states/Apps';
 import { SubscribeHOC } from '../states/helper';
-import styled, { th } from '../styled';
+import styled from '../styled';
 import { Widget } from '../types';
 
 const Title = styled(P.Text)`
@@ -25,6 +24,17 @@ const AppIcon = styled(Image)`
   width: 16px;
   height: 16px;
 `;
+
+const List = styled(View)``;
+const ListItem = styled(View)`
+  padding: 10px;
+  flex-direction: row;
+  border-bottom-color: #aaa;
+  border-bottom-width: 1px;
+`;
+const Left = styled(View)``;
+const Body = styled(View)``;
+const Right = styled(View)``;
 interface State {
   widgets: Widget[];
 }
@@ -72,7 +82,7 @@ export class ManageWidgets extends React.Component<Props, State> {
       .sort((a, b) => {
         return a.attributes.order - b.attributes.order;
       });
-
+    const activeWidgetsShowed = activeWidgets.filter(w => w.attributes.showOnMobile);
     const activeWidgetsNotShowedGrouped = _groupby(
       activeWidgets.filter(w => !w.attributes.showOnMobile),
       'attributes.origin'
@@ -81,13 +91,50 @@ export class ManageWidgets extends React.Component<Props, State> {
     const getIcon = (appKey: string) => appState.getApp(appKey)!.squareLogo;
 
     return (
-      <ScrollView>
-        <P.Container style={{ backgroundColor: '#fff' }}>
-          <StatusBar barStyle="light-content" />
-          <Group>
-            <Title>DASHBOARD</Title>
-            <List style={{ backgroundColor: '#fff' }}>
-              {activeWidgets
+      <P.Container style={{ backgroundColor: '#fff' }}>
+        <StatusBar barStyle="light-content" />
+        <P.H1>Manage</P.H1>
+        <Group>
+          <Title>DASHBOARD</Title>
+          <List style={{ backgroundColor: '#fff' }}>
+            <SortableList
+              sortingEnabled={true}
+              scrollEnabled={false}
+              data={activeWidgetsShowed.reduce((p, cur, i) => {
+                p[cur.attributes.order] = cur;
+                return p;
+              }, {})}
+              order={activeWidgetsShowed.map(w => w.attributes.order)}
+              onChangeOrder={orders => {
+                console.log(orders);
+              }}
+              onPressRow={key => {
+                console.log(key);
+              }}
+              renderRow={({ data: w }, active) => {
+                return (
+                  <ListItem key={w.id}>
+                    {/* <P.Touchable
+                        style={{ flexDirection: 'row' }}
+                        onPress={() => {
+                          this.toggle(w, false);
+                        }}> */}
+                    <Left>
+                      <Ionicons name="ios-remove-circle" size={24} color="#ff3b30" />
+                    </Left>
+                    <Body style={{ flex: 2 }}>
+                      <P.Text>{w.attributes.displayName}</P.Text>
+                    </Body>
+                    <Right>
+                      <AppIcon source={{ uri: getIcon(w.attributes.origin) }} />
+                    </Right>
+                    {/* </P.Touchable> */}
+                  </ListItem>
+                );
+              }}
+            />
+
+            {/* {activeWidgets
                 .filter(w => w.attributes.showOnMobile)
                 .map(w => (
                   <ListItem key={w.id}>
@@ -107,40 +154,33 @@ export class ManageWidgets extends React.Component<Props, State> {
                       </Right>
                     </P.Touchable>
                   </ListItem>
-                ))}
-            </List>
-          </Group>
+                ))} */}
+          </List>
+        </Group>
 
-          {Object.keys(activeWidgetsNotShowedGrouped).map(key => {
-            return (
-              <Group key={key}>
-                <Title>{key.toUpperCase()}</Title>
-                <List style={{ backgroundColor: '#fff' }}>
-                  {activeWidgetsNotShowedGrouped[key].map(w => (
-                    <ListItem key={w.id}>
-                      <P.Touchable
-                        style={{ flexDirection: 'row' }}
-                        onPress={() => {
-                          this.toggle(w, true);
-                        }}>
-                        <Left>
-                          <Ionicons name="ios-add-circle" size={24} color="#4cd964" />
-                        </Left>
-                        <Body style={{ flex: 2 }}>
-                          <P.Text>{w.attributes.displayName}</P.Text>
-                        </Body>
-                        <Right>
-                          <AppIcon source={{ uri: getIcon(w.attributes.origin) }} />
-                        </Right>
-                      </P.Touchable>
-                    </ListItem>
-                  ))}
-                </List>
-              </Group>
-            );
-          })}
-        </P.Container>
-      </ScrollView>
+        {Object.keys(activeWidgetsNotShowedGrouped).map(key => {
+          return (
+            <Group key={key}>
+              <Title>{key.toUpperCase()}</Title>
+              <List style={{ backgroundColor: '#fff' }}>
+                {activeWidgetsNotShowedGrouped[key].map(w => (
+                  <ListItem key={w.id}>
+                    <Left>
+                      <Ionicons name="ios-add-circle" size={24} color="#4cd964" />
+                    </Left>
+                    <Body style={{ flex: 2 }}>
+                      <P.Text>{w.attributes.displayName}</P.Text>
+                    </Body>
+                    <Right>
+                      <AppIcon source={{ uri: getIcon(w.attributes.origin) }} />
+                    </Right>
+                  </ListItem>
+                ))}
+              </List>
+            </Group>
+          );
+        })}
+      </P.Container>
     );
   }
 }
