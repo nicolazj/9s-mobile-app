30f1ac42bce7720f5feb491eabc3231129b15ac8
Nicolas Jiang <nicolas.jiang@9spokes.com>
2019-06-10 16:54:14 +1200

    remove unstated wip

--- src/screens/ManageWidgets.tsx
+++ src/screens/ManageWidgets.tsx
@@ -10,6 +10,7 @@ import SortableRow from '../components/SortableRow';
 import * as P from '../primitives';
 import appState, { AppState } from '../states/Apps';
 import { SubscribeHOC } from '../states/helper';
+import { useOSPStore } from '../stores/osp';
 import styled, { scale } from '../styled';
 import { Widget } from '../types';
 
@@ -44,202 +45,197 @@ const Cell = styled(View)<withChildren>`
   padding: 0 10px;
 `;
 
-interface State {
-  widgets: Widget[];
-  scrollEnabled: boolean;
-}
+const ManageWidgets = () => {
+  const ordersRef = React.useRef<string[]>([]);
+  const [widgets, setWidgets] = React.useState<Widget[]>([]);
+  const [scrollEnabled, setScrollEnabled] = React.useState(true);
+  const { getApp } = useOSPStore();
+  React.useEffect(() => {
+    let current = true;
 
-interface Props {
-  states: [AppState];
-}
-export class ManageWidgets extends React.Component<Props, State> {
-  state = {
-    widgets: [],
-    scrollEnabled: true,
-  } as State;
-  private orders: string[] = [];
-  async componentDidMount() {
-    const widgets = await agent.company.widget.list();
+    const getWidgets = async () => {
+      const widgets = await agent.company.widget.list();
+      if (current) {
+        setWidgets(widgets);
+      }
+    };
+    getWidgets();
 
-    this.setState({ widgets });
-  }
+    return () => {
+      current = false;
+    };
+  }, []);
 
-  toggle = (widget: Widget, show: boolean) => {
+  const toggle = (widget: Widget, show: boolean) => {
     agent.company.widget.updateAttrs(widget.id, {
       order: widget.attributes.order,
       status: widget.attributes.status,
       showOnMobile: show ? '1' : '0',
     });
-    this.setState(state => {
-      return {
-        widgets: state.widgets.map(w =>
-          w.id === widget.id
-            ? {
-                ...w,
-                attributes: {
-                  ...w.attributes,
-                  showOnMobile: show,
-                },
-              }
-            : w
-        ),
-      };
-    });
+
+    setWidgets(
+      widgets.map(w =>
+        w.id === widget.id
+          ? {
+              ...w,
+              attributes: {
+                ...w.attributes,
+                showOnMobile: show,
+              },
+            }
+          : w
+      )
+    );
   };
-  render() {
-    const { widgets, scrollEnabled } = this.state;
-    const [appState_] = this.props.states;
 
-    const activeWidgets = widgets
-      .filter(a => a.attributes.active)
-      .sort((a, b) => {
-        return a.attributes.order - b.attributes.order;
-      });
-    const activeWidgetsShowed = activeWidgets.filter(
-      w => w.attributes.showOnMobile
-    );
-    const activeWidgetsNotShowedGrouped = _groupby(
-      activeWidgets.filter(w => !w.attributes.showOnMobile),
-      'attributes.origin'
-    );
+  const activeWidgets = widgets
+    .filter(a => a.attributes.active)
+    .sort((a, b) => {
+      return a.attributes.order - b.attributes.order;
+    });
+  const activeWidgetsShowed = activeWidgets.filter(
+    w => w.attributes.showOnMobile
+  );
+  const activeWidgetsNotShowedGrouped = _groupby(
+    activeWidgets.filter(w => !w.attributes.showOnMobile),
+    'attributes.origin'
+  );
 
-    const getIcon = (appKey: string) => appState_.getApp(appKey)!.squareLogo;
-    return (
-      <ScrollView scrollEnabled={scrollEnabled}>
-        <P.Container style={{ backgroundColor: '#fff' }}>
-          <StatusBar barStyle="light-content" />
-          <P.H1 style={{ textAlign: 'center' }}>Manage</P.H1>
-          <Group>
-            <P.Title>DASHBOARD</P.Title>
-            <List>
-              <SortableList
-                sortingEnabled={true}
-                scrollEnabled={false}
-                data={activeWidgetsShowed.reduce(
-                  (p: { [key: string]: Widget }, cur, i) => {
-                    p[cur.id] = cur;
-                    return p;
-                  },
-                  {}
-                )}
-                order={activeWidgetsShowed.map(w => w.id)}
-                onChangeOrder={(orders: string[]) => {
-                  this.orders = orders;
-                }}
-                onActivateRow={() => {
-                  this.setState({ scrollEnabled: false });
-                }}
-                onReleaseRow={async () => {
-                  this.setState({ scrollEnabled: true });
-                  const { orders } = this;
-                  const toUpdate = [];
-                  for (let widget of widgets) {
-                    const { id } = widget;
-                    const index = orders.indexOf(id);
-                    if (index > -1) {
-                      widget.attributes.order = index;
-                      toUpdate.push([
-                        widget.id,
-                        {
-                          order: index,
-                          status: widget.attributes.status,
-                          showOnMobile: '1',
-                        },
-                      ]);
-                    }
+  const getIcon = (appKey: string) => getApp(appKey)!.squareLogo;
+  return (
+    <ScrollView scrollEnabled={scrollEnabled}>
+      <P.Container style={{ backgroundColor: '#fff' }}>
+        <StatusBar barStyle="light-content" />
+        <P.H1 style={{ textAlign: 'center' }}>Manage</P.H1>
+        <Group>
+          <P.Title>DASHBOARD</P.Title>
+          <List>
+            <SortableList
+              sortingEnabled={true}
+              scrollEnabled={false}
+              data={activeWidgetsShowed.reduce(
+                (p: { [key: string]: Widget }, cur, i) => {
+                  p[cur.id] = cur;
+                  return p;
+                },
+                {}
+              )}
+              order={activeWidgetsShowed.map(w => w.id)}
+              onChangeOrder={(orders: string[]) => {
+                ordersRef.current = orders;
+              }}
+              onActivateRow={() => {
+                setScrollEnabled(false);
+              }}
+              onReleaseRow={async () => {
+                setScrollEnabled(true);
+                const toUpdate = [];
+                for (let widget of widgets) {
+                  const { id } = widget;
+                  const index = ordersRef.current.indexOf(id);
+                  if (index > -1) {
+                    widget.attributes.order = index;
+                    toUpdate.push([
+                      widget.id,
+                      {
+                        order: index,
+                        status: widget.attributes.status,
+                        showOnMobile: '1',
+                      },
+                    ]);
                   }
+                }
 
-                  this.setState({ widgets });
-                  // need to execute this one by one, otherwise database can not handle it.
-                  for (let payload of toUpdate) {
-                    await agent.company.widget.updateAttrs(
-                      ...(payload as [string, any])
-                    );
-                  }
-                }}
-                renderRow={({
-                  data: widget,
-                  active,
-                }: {
-                  data: Widget;
-                  active: boolean;
-                }) => {
-                  return (
-                    <SortableRow key={widget.id} active={active}>
-                      <ListItem key={widget.id}>
-                        <Cell>
-                          <P.Touchable
-                            onPress={() => {
-                              this.toggle(widget, false);
-                            }}
-                          >
-                            <Ionicons
-                              name="ios-remove-circle"
-                              size={24}
-                              color="#ff3b30"
-                            />
-                          </P.Touchable>
-                        </Cell>
-                        <Cell style={{ flex: 2 }}>
-                          <P.Text>{widget.attributes.displayName}</P.Text>
-                        </Cell>
-                        <Cell>
-                          <AppIcon
-                            source={{ uri: getIcon(widget.attributes.origin) }}
-                          />
-                        </Cell>
-                        <Cell>
-                          <Ionicons name="ios-reorder" size={30} color="#999" />
-                        </Cell>
-                      </ListItem>
-                    </SortableRow>
+                setWidgets(widgets);
+                // need to execute this one by one, otherwise database can not handle it.
+                for (let payload of toUpdate) {
+                  await agent.company.widget.updateAttrs(
+                    ...(payload as [string, any])
                   );
-                }}
-              />
-            </List>
-          </Group>
-
-          {Object.keys(activeWidgetsNotShowedGrouped).map(key => {
-            return (
-              <Group key={key}>
-                <Title>{key.toUpperCase()}</Title>
-                <List>
-                  {activeWidgetsNotShowedGrouped[key].map(widget => (
-                    <P.Touchable
-                      key={widget.id}
-                      onPress={() => {
-                        this.toggle(widget, true);
-                      }}
-                    >
-                      <ListItem key={widget.id}>
-                        <Cell>
+                }
+              }}
+              renderRow={({
+                data: widget,
+                active,
+              }: {
+                data: Widget;
+                active: boolean;
+              }) => {
+                return (
+                  <SortableRow key={widget.id} active={active}>
+                    <ListItem key={widget.id}>
+                      <Cell>
+                        <P.Touchable
+                          onPress={() => {
+                            toggle(widget, false);
+                          }}
+                        >
                           <Ionicons
-                            name="ios-add-circle"
+                            name="ios-remove-circle"
                             size={24}
-                            color="#4cd964"
+                            color="#ff3b30"
                           />
-                        </Cell>
-                        <Cell style={{ flex: 2 }}>
-                          <P.Text>{widget.attributes.displayName}</P.Text>
-                        </Cell>
-                        <Cell>
-                          <AppIcon
-                            source={{ uri: getIcon(widget.attributes.origin) }}
-                          />
-                        </Cell>
-                        <Cell>
-                          <Ionicons name="ios-reorder" size={30} color="#fff" />
-                        </Cell>
-                      </ListItem>
-                    </P.Touchable>
-                  ))}
-                </List>
-              </Group>
-            );
-          })}
-        </P.Container>
-      </ScrollView>
-    );
-  }
-}
-export default SubscribeHOC([appState])(ManageWidgets);
+                        </P.Touchable>
+                      </Cell>
+                      <Cell style={{ flex: 2 }}>
+                        <P.Text>{widget.attributes.displayName}</P.Text>
+                      </Cell>
+                      <Cell>
+                        <AppIcon
+                          source={{ uri: getIcon(widget.attributes.origin) }}
+                        />
+                      </Cell>
+                      <Cell>
+                        <Ionicons name="ios-reorder" size={30} color="#999" />
+                      </Cell>
+                    </ListItem>
+                  </SortableRow>
+                );
+              }}
+            />
+          </List>
+        </Group>
+
+        {Object.keys(activeWidgetsNotShowedGrouped).map(key => {
+          return (
+            <Group key={key}>
+              <Title>{key.toUpperCase()}</Title>
+              <List>
+                {activeWidgetsNotShowedGrouped[key].map(widget => (
+                  <P.Touchable
+                    key={widget.id}
+                    onPress={() => {
+                      toggle(widget, true);
+                    }}
+                  >
+                    <ListItem key={widget.id}>
+                      <Cell>
+                        <Ionicons
+                          name="ios-add-circle"
+                          size={24}
+                          color="#4cd964"
+                        />
+                      </Cell>
+                      <Cell style={{ flex: 2 }}>
+                        <P.Text>{widget.attributes.displayName}</P.Text>
+                      </Cell>
+                      <Cell>
+                        <AppIcon
+                          source={{ uri: getIcon(widget.attributes.origin) }}
+                        />
+                      </Cell>
+                      <Cell>
+                        <Ionicons name="ios-reorder" size={30} color="#fff" />
+                      </Cell>
+                    </ListItem>
+                  </P.Touchable>
+                ))}
+              </List>
+            </Group>
+          );
+        })}
+      </P.Container>
+    </ScrollView>
+  );
+};
+export default ManageWidgets;
